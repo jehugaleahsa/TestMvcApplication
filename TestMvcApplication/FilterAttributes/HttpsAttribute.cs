4571d459be4f3a4be5ae2a5783f6e5f9da4156a3
Travis Parks <jehugaleahsa@gmail.com>
2013-03-29 14:23:30 -0400

    Add some useful utility classes.

---
+++ TestMvcApplication/FilterAttributes/HttpsAttribute.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMvcApplication.FilterAttributes
+{
+    public class HttpsAttribute : ActionFilterAttribute
+    {
+        private readonly bool isEnabled;
+
+        /// <summary>
+        /// Initializes a new instance of a HttpsAttribute.
+        /// </summary>
+        /// <param name="isEnabled">Determines whether the action requires HTTPS.</param>
+        public HttpsAttribute(bool isEnabled)
+        {
+            this.isEnabled = isEnabled;
+        }
+
+        /// <summary>
+        /// Called by the ASP.NET MVC framework before the action method executes.
+        /// </summary>
+        /// <param name="filterContext">The filter context.</param>
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            HttpRequestBase request = filterContext.HttpContext.Request;
+            HttpResponseBase response = filterContext.HttpContext.Response;
+
+            if (!request.IsLocal)
+            {
+                if (!request.IsSecureConnection && isEnabled)
+                {
+                    Uri uri = request.Url;
+                    UriBuilder builder = new UriBuilder(uri)
+                    {
+                        Scheme = Uri.UriSchemeHttps,
+                        Port = 443,
+                    };
+                    string url = builder.Uri.ToString();
+                    filterContext.Result = new RedirectResult(url);
+                }
+                else if (request.IsSecureConnection && !isEnabled)
+                {
+                    Uri uri = request.Url;
+                    UriBuilder builder = new UriBuilder(uri)
+                    {
+                        Scheme = Uri.UriSchemeHttp,
+                        Port = 80,
+                    };
+                    string url = builder.Uri.ToString();
+                    filterContext.Result = new RedirectResult(url);
+                }
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
---
+++ TestMvcApplication/Helpers/DisplayAttributeExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace TestMvcApplication.Helpers
+{
+    public static class DisplayAttributeExtensions
+    {
+        public static MvcHtmlString SetAttribute<TModel, TProp>(this HtmlHelper<TModel> helper, string attribute, Func<TModel, TProp> propertySelector)
+        {
+            TModel model = helper.ViewData.Model;
+            if (model == null)
+            {
+                return MvcHtmlString.Empty;
+            }
+            TProp value = propertySelector(model);
+            string result = String.Format(@"{0}=""{1}""", attribute, value);
+            return MvcHtmlString.Create(result);
+        }
+
+        public static MvcHtmlString SetFlagAttribute<TModel>(this HtmlHelper<TModel> helper, string attribute, Func<TModel, bool> propertySelector)
+        {
+            TModel model = helper.ViewData.Model;
+            bool isEnabled = propertySelector(model);
+            if (isEnabled)
+            {
+                string result = String.Format(@"{0}=""{0}""", attribute);
+                return MvcHtmlString.Create(result);
+            }
+            else
+            {
+                return MvcHtmlString.Empty;
+            }
+        }
+    }
+}
