482b6032298a3eeeca5c6b62b2a0fba61703a9d5
Travis Parks <jehugaleahsa@gmail.com>
2014-04-13 09:56:06 -0400

    Support loading related entities explicitly.
    Using POCOs without lazy loading means there's no direct way to retrieve
    related entities from the database. To allow grabbing related entities
    dynamically, I created an EntityLoader class that accepts an expression
    tree.

---
+++ DataModeling/EntityLoader.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using ServiceInterfaces;
+
+namespace DataModeling
+{
+    public class EntityLoader<TEntity> : IEntityLoader<TEntity>
+        where TEntity : class
+    {
+        private readonly DbContext context;
+        private readonly TEntity entity;
+
+        internal EntityLoader(DbContext context, TEntity entity)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            this.context = context;
+            this.entity = entity;
+        }
+
+        public void Reload()
+        {
+            DbEntityEntry<TEntity> entry = context.Entry(entity);
+            entry.Reload();
+        }
+
+        public bool IsLoaded<TRelation>(Expression<Func<TEntity, TRelation>> accessor)
+            where TRelation : class
+        {
+            DbEntityEntry<TEntity> entry = context.Entry(entity);
+            DbReferenceEntry reference = entry.Reference(accessor);
+            return reference.IsLoaded;
+        }
+
+        public TRelation Load<TRelation>(Expression<Func<TEntity, TRelation>> accessor)
+            where TRelation : class
+        {
+            DbEntityEntry<TEntity> entry = context.Entry(entity);
+            DbReferenceEntry reference = entry.Reference(accessor);
+            if (!reference.IsLoaded)
+            {
+                reference.Load();
+            }
+            Func<TEntity, TRelation> compiled = accessor.Compile();
+            return compiled(entity);
+        }
+
+        public bool IsLoaded<TRelation>(Expression<Func<TEntity, ICollection<TRelation>>> accessor)
+            where TRelation : class
+        {
+            DbEntityEntry<TEntity> entry = context.Entry(entity);
+            DbCollectionEntry collection = entry.Collection(accessor);
+            return collection.IsLoaded;
+        }
+
+        public ICollection<TRelation> Load<TRelation>(Expression<Func<TEntity, ICollection<TRelation>>> accessor)
+            where TRelation : class
+        {
+            DbEntityEntry<TEntity> entry = context.Entry(entity);
+            DbCollectionEntry collection = entry.Collection(accessor);
+            if (!collection.IsLoaded)
+            {
+                collection.Load();
+            }
+            Func<TEntity, ICollection<TRelation>> compiled = accessor.Compile();
+            return compiled(entity);
+        }
+
+        public IQueryable<TRelation> LoadQuery<TRelation>(Expression<Func<TEntity, TRelation>> accessor)
+            where TRelation : class
+        {
+            DbEntityEntry<TEntity> entry = context.Entry(entity);
+            DbReferenceEntry<TEntity, TRelation> reference = entry.Reference(accessor);
+            return reference.Query();
+        }
+
+        public IQueryable<TRelation> LoadQuery<TRelation>(Expression<Func<TEntity, ICollection<TRelation>>> accessor)
+            where TRelation : class
+        {
+            DbEntityEntry<TEntity> entry = context.Entry(entity);
+            DbCollectionEntry<TEntity, TRelation> collection = entry.Collection(accessor);
+            return collection.Query();
+        }
+    }
+}
---
+++ ServiceInterfaces/IEntityLoader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ServiceInterfaces
+{
+    public interface IEntityLoader<TEntity>
+        where TEntity : class
+    {
+        bool IsLoaded<TRelation>(Expression<Func<TEntity, ICollection<TRelation>>> accessor) where TRelation : class;
+
+        bool IsLoaded<TRelation>(Expression<Func<TEntity, TRelation>> accessor) where TRelation : class;
+
+        ICollection<TRelation> Load<TRelation>(Expression<Func<TEntity, ICollection<TRelation>>> accessor) where TRelation : class;
+
+        TRelation Load<TRelation>(Expression<Func<TEntity, TRelation>> accessor) where TRelation : class;
+
+        IQueryable<TRelation> LoadQuery<TRelation>(Expression<Func<TEntity, ICollection<TRelation>>> accessor) where TRelation : class;
+
+        IQueryable<TRelation> LoadQuery<TRelation>(Expression<Func<TEntity, TRelation>> accessor) where TRelation : class;
+
+        void Reload();
+    }
+}
